27aa4eeef8306389fd7077355d90f0e607899197
Anyaegbu Francis <anyaegbufrancis@gmail.com>
2020-08-17 02:07:25 +0400

    base class initiaization

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee  {
+    constructor (name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    get getName(name) {
+        return this.name
+    }
+    get getName(id) {
+        return this.id
+    }
+    get getName(email) {
+        return this.email
+    }    
+}
+
+
+
+// class Manager extends Employee {
+//     constructor (officeNumber) {
+//         this.officeNumber = officeNumber
+//     }    
+// }
